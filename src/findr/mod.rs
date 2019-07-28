de74b3ec63a0dad1b101fd936a974d9f50f12450
Rob Wong <dev.wonger@gmail.com>
2019-07-28 14:06:44 -0600

    brb

--- src/findr/mod.rs
+++ src/findr/mod.rs
@@ -1,34 +1,18 @@
-use std::fs::{read_dir, read_to_string, DirEntry, File};
+use std::fs::{read_dir,  DirEntry};
 use std::io;
-use std::io::Read;
 use std::path::Path;
 
-pub fn visit_dirs(dir: &Path, search_str: &String) -> io::Result<()> {
+pub fn visit_dirs(dir: &Path, search_str: &String, cb: &Fn(&DirEntry)) -> io::Result<()> {
     if dir.is_dir() {
         for entry in read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
-                visit_dirs(&path, search_str)?;
-            } else {
-                file_match(&entry, search_str);
+                visit_dirs(&path, search_str, cb)?;
+            } else if entry.file_name().into_string().unwrap().contains(search_str) {
+                cb(&entry);
             }
         }
     }
     Ok(())
-}
-
-// example of something to do when a file is found
-fn file_match(d: &DirEntry, search_str: &String) {
-    if d.file_name().into_string().unwrap().contains(search_str) {
-        let mut f = File::open(d.path()).unwrap();
-        let mut s = String::new();
-        match f.read_to_string(&mut s) {
-            Ok(_) => {
-                println!("{:?}", s);
-            },
-            _ => (),
-        }
-        println!("{:?} matched", d.file_name().into_string());
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@
 
 use std::env;
 use std::path::Path;
+use std::fs::{File};
+use std::io::Read;
 
 mod findr;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    match findr::visit_dirs(Path::new(&args[1]), &args[2]) {
+    match findr::visit_dirs(Path::new(&args[1]), &args[2], &|d| {
+        let mut f = File::open(d.path()).unwrap();
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Ok(_) => {
+                println!("{:?}", s);
+            },
+            _ => (),
+        }
+        println!("{:?} matched", d.file_name().into_string());
+    }) {
       Ok(_) => println!("done"),
       Err(e) => panic!(e)
     }
